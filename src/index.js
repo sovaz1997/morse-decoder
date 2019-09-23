1ae6e8ecaa8e28a4ecd2b7742a77c1a67e237886
Oleg Smirnov <sovaz1997@yandex.ru>
2019-09-23 08:57:07 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -37,8 +37,36 @@ const MORSE_TABLE = {
     '-----':  '0',
 };
 
+function getLetter(expr) {
+    if(expr[0] == '*') {
+        return ' ';
+    }
+
+    let regexpPos = 0;//expr.search('/1*/');
+    for(; regexpPos < expr.length && expr[regexpPos] != '1'; regexpPos++);
+    
+    expr = expr.substr(regexpPos, expr.length - regexpPos);
+
+    let res = '';
+    
+    for(let i = 0; i < expr.length; i += 2) {
+        if(expr.substr(i, 2) == '10') {
+            res += '.';
+        } else if(expr.substr(i, 2) == '11') {
+            res += '-';
+        }
+    }
+
+    return MORSE_TABLE[res];
+}
+
 function decode(expr) {
-    // write your solution here
+    res = '';
+    for(let i = 0; i < expr.length; i += 10) {
+        let sub = expr.substr(i, 10);
+        res += getLetter(sub);
+    }
+    return res;
 }
 
 module.exports = {
